8ef83ac5e993da08267c5bbb1cbbd7eba8610cd2
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-03 18:13:25 +0700

    add bique5200

--- paoshu8/src/home.js
+++ paoshu8/src/home.js
@@ -6,9 +6,7 @@ function execute() {
         {title: "都市小说", input: "http://www.paoshu8.com/dushixiaoshuo/", script: "gen.js"},
         {title: "穿越小说", input: "http://www.paoshu8.com/chuanyuexiaoshuo/", script: "gen.js"},
         {title: "网游小说", input: "http://www.paoshu8.com/wangyouxiaoshuo/", script: "gen.js"},
-        {title: "科幻小说", input: "http://www.paoshu8.com/kehuanxiaoshuo/", script: "gen.js"},
-        {title: "排行榜单", input: "http://www.paoshu8.com/paihangbang/", script: "gen.js"},
-        {title: "全部小说", input: "http://www.paoshu8.com/xiaoshuodaquan/", script: "gen.js"}
+        {title: "科幻小说", input: "http://www.paoshu8.com/kehuanxiaoshuo/", script: "gen.js"}
 
     ]);
 }
