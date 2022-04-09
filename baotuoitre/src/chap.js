8e855a51294fcb932b489c35ee46aeb1fe5d05a4
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-09 22:10:07 +0700

    test

---
+++ baotuoitre/src/chap.js
@@ -0,0 +1,14 @@
+function execute(url) {
+    let response = fetch(url);
+    if (response.ok) {
+        let doc = response.html();
+        let htm = doc.select("div.main-content-body");
+        // htm.select("h2").remove();
+        htm.select(".relate-container").remove();
+        // htm.select(".VCSortableInPreviewMode").remove();
+        htm.select("a").remove();
+        htm = htm.html().replace("<i class=\"icon-pencil\"><\/i>","").replace(/\n/g,'').replace("<h2","<b").replace("<\/h2>","<\/b>");
+        return Response.success(htm);
+    }
+    return null;
+}
--- coloredmanga/src/genre.js
+++ coloredmanga/src/genre.js
@@ -1,5 +1,5 @@
 function execute() {
-    const doc = Http.get("https://coloredmanga.com").html();
+    const doc = Http.get("https://coloredmanga.com/blog/manga-genre/action/").html();
     const el = doc.select(".genres ul > li a");
     const data = [];
     for (var i = 3; i < el.size(); i++) {
