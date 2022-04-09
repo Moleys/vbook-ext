ad943289b83e6a74d68391924cbbf4306fdef652
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-09 22:23:19 +0700

    test

--- baotuoitre/src/chap.js
+++ baotuoitre/src/chap.js
@@ -5,7 +5,7 @@ function execute(url) {
         let htm = doc.select("div.main-content-body");
         // htm.select("h2").remove();
         htm.select(".relate-container").remove();
-        // htm.select(".VCSortableInPreviewMode").remove();
+        htm.select('div[type="RelatedOneNews"]').remove();
         htm.select("a").remove();
         htm = htm.html().replace("<i class=\"icon-pencil\"><\/i>","").replace(/\n/g,'').replace("<h2","<b").replace("<\/h2>","<\/b>");
         return Response.success(htm);
