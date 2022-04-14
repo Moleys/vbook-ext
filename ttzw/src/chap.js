56426fb9ecbc602ca950e756e1572da7340ac963
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-14 17:07:38 +0700

    fix ttzw

--- ttzw/src/chap.js
+++ ttzw/src/chap.js
@@ -1,15 +1,28 @@
 function execute(url) {
     url = url.replace('m.ttzw.com', 'www.ttzw.com');
-    let response = fetch(url);
 
-    if (response.ok) {
-        let doc = response.html();
-        doc.select(".posterror").remove();
-        doc.select("script").remove();
+    let cvData ="";
+    let part1 = url.replace("http://www.ttzw.com", "").replace("https://www.ttzw.com", "").replace(".html","");
+    var next = part1;
+    while (next.includes(part1)) {
+        let response = fetch("http://www.ttzw.com" + next +".html");
 
-        let htm = doc.select("#content").html();
-        htm = htm.replace(/\&nbsp;/g, "");
-        return Response.success(htm);
+        if (response.ok) {
+            let doc = response.html();
+
+            next = doc.select(".section-opt").last().select("a").get(2).attr("href").replace(".html","");
+            console.log(next)
+            doc.select(".posterror").remove();
+            doc.select("script").remove();
+            let htm = doc.select("#content").html();
+            htm = htm.replace(/\&nbsp;/g, "");
+            cvData = cvData + htm;
+        } else {
+            break;
+        }
+    }
+    if (cvData) {
+        return Response.success(cvData);
     }
     return null;
 }
