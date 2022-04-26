2cde9754cd7b4f5245365474f40aef6657ba4d92
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-26 22:47:42 +0700

    fix xswang

--- xswang/src/chap.js
+++ xswang/src/chap.js
@@ -4,9 +4,13 @@ function execute(url) {
 
     if (response.ok) {
         let doc = response.html();
-        let htm = doc.select("#content").html();
-        htm = htm.replace(/\&nbsp;/g, "");
+        let data = []
+        doc.select("#content p.content_detail").forEach(e => {
+            data.push(e.text().replace("首发网址ｈｔｔps://m.ｘｓｗａng.com ","").replace(" 一秒记住ｈｔｔｐs://ｍ．ｘｓｗａｎｇ.ｃｏｍ",""));
+        });
+        let htm = data.join("<br>");
         return Response.success(htm);
+        
     }
     return null;
 }
