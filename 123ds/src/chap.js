b16f34ba68b0ceceb06de068bb05bbbdcce71678
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-27 21:28:22 +0700

    fix min-yuan

--- 123ds/src/chap.js
+++ 123ds/src/chap.js
@@ -1,11 +1,19 @@
 function execute(url) {
-    url = url.replace('m.paoshu8.com', 'www.paoshu8.com');
     let response = fetch(url);
 
     if (response.ok) {
         let doc = response.html();
-        let htm = doc.select("#content").html();
-        htm = htm.replace(/\&nbsp;/g, "");
+        let htm = doc.select("div#DivContentBG");
+        htm.select("div.DivCurrentPos").remove()
+        htm.select("script").remove()
+        htm.select("h1").remove()
+        htm.select("hr").remove()
+        htm.select("center").remove()
+        htm.select("select").remove()
+        htm.select('meta[property="text-align:center"]')
+        console.log(htm)
+
+        htm = htm.html().replace(/\&nbsp;/g, "").replace(/\&emsp;/g, "");
         return Response.success(htm);
     }
     return null;
--- min-yuan/src/chap.js
+++ min-yuan/src/chap.js
@@ -1,12 +1,24 @@
+
 function execute(url) {
     url = url.replace('m.min-yuan.com', 'www.min-yuan.com');
-    let response = fetch(url);
-
-    if (response.ok) {
-        let doc = response.html();
-        let htm = doc.select("#content").html();
-        htm = htm.replace(/\&nbsp;/g, "");
-        return Response.success(htm);
+    let data  ="";
+    let part1 = url.replace("http://www.min-yuan.com", "").replace(".html","");
+    var next = part1;
+    while (next.includes(part1)) {
+        console.log(next)
+        let response = fetch("http://www.min-yuan.com" + next +".html");
+        if (response.ok) {
+            let doc = response.html();
+            next = doc.select(".g-content-nav a").last().attr("href").replace(".html","");
+console.log(next)
+            let htm = doc.select("#content").html();
+            data = data + htm;
+        } else {
+            break;
+        }
+    }
+    if (data) {
+        return Response.success(data);
     }
     return null;
 }
