c794ef0413e9fad706c627ed177626242d0c8d97
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-27 16:51:09 +0700

    fix biqiudu

--- biqiudu/src/chap.js
+++ biqiudu/src/chap.js
@@ -5,7 +5,7 @@ function execute(url) {
     if (response.ok) {
         let doc = response.html();
         let htm = doc.select("#content").html();
-        htm = htm.replace(/\&nbsp;/g, "");
+        htm = htm.replace(/\&nbsp;/g, "").replace("江浙湖汉北 ｈttpｓ://m.biqiudu.com笔趣阁","").replace("ｈttpｓ://m.biqiudu.com笔趣阁","").replace("笔趣阁网址ｍ．biqiudu。com","").replace("一秒记住ｍ．biqiudu．com","").replace("OTg2NTc=","");
         return Response.success(htm);
     }
     return null;
