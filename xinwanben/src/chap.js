d1ac7a78915fbe00938ef138c3b3a413e781d020
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-28 18:58:08 +0700

    fix xinwanben

--- xinwanben/src/chap.js
+++ xinwanben/src/chap.js
@@ -1,12 +1,27 @@
 function execute(url) {
     url = url.replace('m.xinwanben.com', 'www.xinwanben.com');
-    let response = fetch(url);
 
-    if (response.ok) {
-        let doc = response.html();
-        let htm = doc.select("#content").html();
-        htm = htm.replace(/\&nbsp;/g, "");
-        return Response.success(htm);
+    let data  ="";
+    let part1 = url.replace("https://www.xinwanben.com", "").replace("http://www.xinwanben.com", "").replace(".html","");
+
+    var next = part1;
+
+        console.log(next)
+    while (next.includes(part1)) {
+        console.log(next)
+        let response = fetch("http://www.xinwanben.com" + next +".html");
+        if (response.ok) {
+            let doc = response.html();
+            next = doc.select("a#next_url").last().attr("href").replace(".html","");
+console.log(next)
+            let htm = doc.select("#content").html();
+            data = data + htm;
+        } else {
+            break;
+        }
+    }
+    if (data) {
+        return Response.success(data);
     }
     return null;
 }
