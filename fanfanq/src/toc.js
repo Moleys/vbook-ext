56c5af3eef4de3d387769ef318ef70f7a7df58d6
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-29 09:54:32 +0700

    add emoji to forum site

--- fanfanq/src/toc.js
+++ fanfanq/src/toc.js
@@ -3,12 +3,13 @@ function execute(url) {
     let response0 = fetch(url);
     if (response0.ok) {
         let doc0 = response0.html('gbk');
-        let url1 = "https://bbs.fanfanq.com/" +doc0.select('#read_Att_tpc a[style*="color:green;"]').last().attr("href");
+        let url1 = "https://bbs.fanfanq.com/" +doc0.select("a:contains(在线阅读已开放)").first().attr("href");
+        if(url1.slice(-1) !== "/")
+            url1 = url1 + "/";
         let response1 = fetch(url1);
         if (response1.ok) {
             let doc = response1.html('gbk');
-            let el1 = doc.select("ul.cf")
-            let el = el1.select("li a")
+            let el = doc.select("ul.cf li a")
             const data = [];
             for (let i = 0;i < el.size(); i++) {
                 var e = el.get(i);
