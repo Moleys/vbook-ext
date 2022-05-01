e9c6b8d7e53758620f99ed8702a60459ae2e13b8
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-05-01 16:15:41 +0700

    add genre yuanshuku

--- yuanshuku/src/detail.js
+++ yuanshuku/src/detail.js
@@ -8,12 +8,13 @@ function execute(url) {
         if (coverImg.startsWith("/")) {
             coverImg = "http://www.yuanshuku.com" + coverImg;
         }
+        let author  = doc.select("#info p").first().text().replace(/作\s*者：/g, "");
         return Response.success({
             name: doc.select("#info h1").text(),
             cover: coverImg,
-            author: doc.select("#info p").first().text().replace(/作\s*者：/g, ""),
+            author: author,
             description: doc.select("#intro").text(),
-            detail: doc.select("#info p").last().html(),
+            detail: "作者：" + author + "<br>" +doc.select("#info p").last().html(),
             host: "http://www.yuanshuku.com"
         });
     }
--- yuanshuku/src/home.js
+++ yuanshuku/src/home.js
@@ -1,22 +1,5 @@
 function execute() {
     return Response.success([
         {title: "首页", input: "http://www.yuanshuku.com/", script: "gen.js"},
-        {title: "玄幻", input: "http://www.yuanshuku.com/sort/1_1/", script: "gen.js"},
-        {title: "武侠", input: "http://www.yuanshuku.com/sort/2_1/", script: "gen.js"},
-        {title: "都市", input: "http://www.yuanshuku.com/sort/3_1/", script: "gen.js"},
-        {title: "言情", input: "http://www.yuanshuku.com/sort/4_1/", script: "gen.js"},
-        {title: "历史", input: "http://www.yuanshuku.com/sort/5_1/", script: "gen.js"},
-        {title: "科幻", input: "http://www.yuanshuku.com/sort/6_1/", script: "gen.js"},
-        {title: "恐怖", input: "http://www.yuanshuku.com/sort/7_1/", script: "gen.js"},
-        {title: "其他", input: "http://www.yuanshuku.com/sort/8_1/", script: "gen.js"},
-        {title: "穿越", input: "http://www.yuanshuku.com/sort/9_1/", script: "gen.js"},
-        {title: "游戏", input: "http://www.yuanshuku.com/sort/10_1/", script: "gen.js"},
-        {title: "轻小说", input: "http://www.yuanshuku.com/sort/11_1/", script: "gen.js"},
-        {title: "古言", input: "http://www.yuanshuku.com/sort/12_1/", script: "gen.js"},
-        {title: "二次元", input: "http://www.yuanshuku.com/sort/13_1/", script: "gen.js"},
-        {title: "青春", input: "http://www.yuanshuku.com/sort/14_1/", script: "gen.js"},
-        {title: "同人", input: "http://www.yuanshuku.com/sort/15_1/", script: "gen.js"},
-        {title: "GLBL", input: "http://www.yuanshuku.com/sort/16_1/", script: "gen.js"}
-
     ]);
 }
