5b2541315d1da6658ef26414eacb19b4c00d6c5a
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-05-01 13:12:43 +0700

    add search Jjwxc

--- jjwxc/src/search.js
+++ jjwxc/src/search.js
@@ -1,40 +1,22 @@
 function execute(key, page) {
     if (!page) page = '1';
-    let response = fetch('http://www.jjwxc.net/search.php', {
-        method: "GET",
-        queries: {
-            kw : key,
-            t : 1,
-            p : page
-        }
-    });
-
+    key = encodeURI(key)
+    let response = fetch('http://app.jjwxc.net/androidapi/search?keyword=' + key + '&type=1&page=1&token=null&searchType=1&sortMode=DESC&versionCode=133`');
     if (response.ok) {
-        let doc = response.html('gbk');
-        const data = [];
-		let elePage = doc.select("#search_result div.page").first();
-        let next = ''
-        if(elePage.text().length>0){
-            elePage.select("a").last().remove();
-            next  = elePage.select("a").last().attr("href").split("p=")[1];
-        }
-		let eleSearch = doc.select("#search_result div");
-        eleSearch.forEach(e => {
-            let name = e.select("h3.title").first().text();
-            let author = e.select(".info a").first().text();
-            let link = e.select("h3.title a").first().attr("href")
-            if(name !== null && name !== '') {
-                data.push({
-                    name: name,
-                    link: link,
-                    detail: author,
-                    host: "http://botruyen.com"
-                });     
-            }
-        });
+        let items = response.json().items;
+        let array = []
 
+        items.forEach((item) => {
+          array.push({
+              name: item.novelname,
+              detail: item.authorname,
+              cover: item.cover,
+              link: "http://www.jjwxc.net/onebook.php?novelid=" +item.novelid,
+              host: "http://www.jjwxc.net"
+          })
+      })
 
-        return Response.success(data, next.toString());
+        return Response.success(array);
     }
     return null;
 }
