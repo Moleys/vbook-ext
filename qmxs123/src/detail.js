52ff05e24860cf8eb81932fcbe9b5e0a84f12784
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-05-12 18:41:34 +0700

    update qmxs123

--- qmxs123/src/detail.js
+++ qmxs123/src/detail.js
@@ -8,12 +8,13 @@ function execute(url) {
         if (coverImg.startsWith("/")) {
             coverImg = "https://www.qmxs123.com" + coverImg;
         }
+        let author = doc.select(".small span").first().text().replace(/作\s*者：/g, "")
         return Response.success({
             name: doc.select("h1").text(),
             cover: coverImg,
-            author: doc.select(".small span").first().text().replace(/作\s*者：/g, ""),
+            author: author,
             description: doc.select(".intro").text(),
-            detail: doc.select(".small span.last").first().html(),
+            detail: "作者：" + author + "<br>" + doc.select(".small span.last").first().html(),
             host: "https://www.qmxs123.com"
         });
     }
--- qmxs123/src/home.js
+++ qmxs123/src/home.js
@@ -1,15 +1,5 @@
 function execute() {
     return Response.success([
-        {title: "首页", input: "https://www.qmxs123.com/", script: "gen.js"},
-        {title: "玄幻", input: "https://www.qmxs123.com/xuanhuan/", script: "gen.js"},
-        {title: "武侠", input: "https://www.qmxs123.com/wuxia/", script: "gen.js"},
-        {title: "都市", input: "https://www.qmxs123.com/dushi/", script: "gen.js"},
-        {title: "历史", input: "https://www.qmxs123.com/lishi/", script: "gen.js"},
-        {title: "网游", input: "https://www.qmxs123.com/wangyou/", script: "gen.js"},
-        {title: "科幻", input: "https://www.qmxs123.com/kehuan/", script: "gen.js"},
-        {title: "女生", input: "https://www.qmxs123.com/mm/", script: "gen.js"},
-        {title: "完本", input: "https://www.qmxs123.com/finish/", script: "gen.js"},
-        {title: "排行榜", input: "https://www.qmxs123.com/top/", script: "gen.js"}
-
+        {title: "首页", input: "https://www.qmxs123.com/", script: "gen.js"}
     ]);
 }
