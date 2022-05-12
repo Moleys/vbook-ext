867832e96766a3820bec879efca1b646634ba251
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-05-12 19:19:25 +0700

    fix wdbqg

--- wdbqg/src/detail.js
+++ wdbqg/src/detail.js
@@ -8,12 +8,13 @@ function execute(url) {
         if (coverImg.startsWith("/")) {
             coverImg = "http://www.wdbqg.com" + coverImg;
         }
+        let author  = doc.select(".info p").first().text().replace(/作\s*者：/g, "");
         return Response.success({
             name: doc.select(".info h1").text(),
             cover: coverImg,
-            author: doc.select(".info p").first().text().replace(/作\s*者：/g, ""),
-            description: doc.select(".desc").first().text(),
-            detail: doc.select(".info p").get(4).text(),
+            author: author,
+            description: doc.select("#intro").text(),
+            detail: "作者：" + author + "<br>" +doc.select(".info p").last().text(),
             host: "http://www.wdbqg.com"
         });
     }
--- wdbqg/src/toc.js
+++ wdbqg/src/toc.js
@@ -3,8 +3,7 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        let el1 = doc.select(".section-box").last();
-        let el = el1.select("li a")
+        let el = doc.select(".section-box").last().select("li a")
         const data = [];
         for (let i = 0;i < el.size(); i++) {
             var e = el.get(i);
