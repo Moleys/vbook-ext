b5a7883bcacbfd8323f6892d9f9c661d1cb6c120
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-06-10 22:08:29 +0700

    chivicn

--- chivicn/src/page.js
+++ chivicn/src/page.js
@@ -1,10 +1,15 @@
 function execute(url) {
     url = url.replace("chivi.xyz", "chivi.app");
     let response = fetch(url + "/chaps");
+    
 
     if (response.ok) {
         let doc = response.html();
-        let story = doc.html().match(/api\/chaps\/(\d+\/[a-z_]+)/)[1];
+        let story = doc.html().split('<script type=\"application\/json\" sveltekit:data-type=\"data\" sveltekit:data-url=\"')[2].split('"')[0].split('?pg=')[0];
+
+        // console.log(story)
+
+
         let lastPage = doc.html().match(/pgmax.*?:(\d+)/);
  
         if (lastPage) {
@@ -14,7 +19,7 @@ function execute(url) {
         }
         const pageList = [];
         for (let i = 1; i <= lastPage; i++) {
-            pageList.push(url + " https://chivi.app/api/chaps/" + story + "?pg=" + i);
+            pageList.push(url + " https://chivi.app" + story + "?pg=" + i);
         }
 
         return Response.success(pageList);
