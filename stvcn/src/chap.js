2dccf8e54209f7ff02049b33b9eef3e4f4f787a4
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-06-26 18:11:32 +0700

    test  stv

--- stvcn/src/chap.js
+++ stvcn/src/chap.js
@@ -1,68 +1,82 @@
 function execute(url) {
     if(url.slice(-1) !== "/")
         url = url + "/";
-    let el1 =""
+        let el1 =""
     var browser = Engine.newBrowser() // Khởi tạo browser
-    let doc = browser.launch(url, 5500) // Mở trang web với timeout, trả về Document object
-    el1 = doc.select(".contentbox")
-    el1.select("i[hd=\"\"]").remove()
-
+    let doc = browser.launch(url, 2200) // Mở trang web với timeout, trả về Document object
+    el1 = browser.callJs("document.getElementsByTagName('body')[0].innerHTML = document.cookie;", 100);
+    el1 = el1.select("body").text()
     browser.close()
-    // console.log(el1)
-    el1.select("img").remove()
-    el1.select("span").remove()
-    el1 = el1.html()
-    el1 = el1.replace(/\"/g,"'")
-    // const regex1 = /h='(.*?)'/g;
-    // const regex2 = /p='(.*?)'/g;
-    // const regex3 = /v='(.*?)'/g;
-    // const regex4 = /id='(.*?)'/g;
-    // const regex5 = /<it='(.*?)'>(.*?)<\/i>/g;
-    
-
-    // el1 = el1.replace(regex1, "").replace(regex2, "").replace(regex3, "").replace(regex4, "").replace(/ /g,"").replace(regex5, "$1").replace("Đọctrênwebđểcóchấtlượngdịchcaovàủnghộwebsite.", "")
-
     console.log(el1)
 
-    const regex1 = /<i h='(.*?)' t='(.*?)' v='(.*?)' p='(.*?)' id='(.*?)'>(.*?)<\/i>/g;
-    const regex2 = /<i (.*?)>(.*?)<\/i>/g;
-    el1 = el1.replace(regex1, "$2").replace(regex2, "").replace(/ /g,"").replace("Đọctrênwebđểcóchấtlượngdịchcaovàủnghộwebsite.", "")
+    let host = url.split('/truyen/')[0];
+    let params = url.split('/truyen/')[1].split('/');
+    let bookid = params[2];
+    let currentid = params[3];
+    let bookhost = params[0];
+    let booksty = params[1];
+    let currentidc = '';
 
+    let response = fetch(host+'/index.php',{
+        method: "POST",
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+            'Cookie': el1,
+            'Referer': url
+        },
+        queries: {
+            sajax : 'readchapter',
+            bookid : bookid,
+            h : bookhost,
+            c2 : currentidc,
+            c : currentid,
+            sty : booksty,
+            exts : ""  
+        }
+    });
 
-    // el1 = el1.replace(/\"/g,"'")
-    // content = el1.replace(/<a href=.*?<\/a>/g, "");
-    // const regex1 = /<i h='(.*?)'t='(.*?)'v='(.*?)'p='(.*?)'>(.*?)<\/i>/g;
-    // content = content.replace(regex1, "$2").replace(/ /g,"").replace(/\n/g, "<br>");
-    // content = content.replace(/<(\/)?i.*?>/g, "");
-    // content = content.replace(/<span.*?>(.*?)<\/span>(<br>)?/g, "");
-    // content = content.replace(/(\n)?\t/g, "<br>");
-    // content = content.replace(/\s{2,}/g, " ");
-    // if (url.indexOf("bxwxorg") > 0) {
-    //     content = content.replace(/<\/?p.*?>/g, "");
-    //     content = content.replace(/(Ta chiếm được.*:?)/g, "");
-    // }
-    // if (url.indexOf("uukanshu") > 0) {
-    //     content = content.replace("UUKANSHU đọc sách www.uukanshu.com", "");
-    //     content = content.replace(/<div.*?>(.*?)<\/div>/g, "");
-    //     content = content.replace(/<p><\/p>\t<br>/g,"");
-    //     content = content.replace(/<\/div>/g,"");
-    //     content = content.replace(/\(https.*<br>/g,"");
-    // }
-    // if (url.indexOf("aikanshu") > 0) {
-    //     content = content.replace(/<img.*?src="\/novel\/images.*?>/g, "");
-    // }
-    // if (url.indexOf("ciweimao") > 0) {
-    //     content = content.replace(/<span>.*?<\/span>/g, "");
-    //     content = content.replace(/<\/?p.*?>/g, "");
-    // }
-    // content = content.replace(/&(nbsp|amp|quot|lt|gt|bp|emsp);/g, "");
-    // content = content.replace(/(<br\/?>)+/g,"<br>");
-    // content = content.replace(/<\/p><br><br><p>/g,"<br><br>");
-    // content = content.replace(/ ([,\.!\?”]+)/g,"$1");
-    // content = content.replace(/ ([,\.!\?”]+)/g,"$1");
+    if(response.ok){
+        let res = response.json()
+        if(!res){          
+            return Response.success("Không có nội dung");
+        }
 
-    //Đã lọc rác có thể. còn nhiều vcl ra ý. tự lọc đi
-    return Response.success(el1);
+        var content = res.data;
+        content = content.replace(/<a href=.*?<\/a>/g, "");
+        content = content.replace(/<i (.*?)' hd >(.*?)<\/i>/g, "");
+        const regex1 = /<i h='(.*?)'t='(.*?)'v='(.*?)'p='(.*?)'>(.*?)<\/i>/g;
+        content = content.replace(regex1, "$2").replace(/ /g,"").replace(/\n/g, "<br>");
+        content = content.replace(/<(\/)?i.*?>/g, "");
+        content = content.replace(/<span.*?>(.*?)<\/span>(<br>)?/g, "");
+        content = content.replace(/(\n)?\t/g, "<br>");
+        content = content.replace(/\s{2,}/g, " ");
+
+        if (url.indexOf("bxwxorg") > 0) {
+            content = content.replace(/<\/?p.*?>/g, "");
+            content = content.replace(/(Ta chiếm được.*:?)/g, "");
+        }
+        if (url.indexOf("uukanshu") > 0) {
+            content = content.replace("UUKANSHU đọc sách www.uukanshu.com", "");
+            content = content.replace(/<div.*?>(.*?)<\/div>/g, "");
+            content = content.replace(/<p><\/p>\t<br>/g,"");
+            content = content.replace(/<\/div>/g,"");
+            content = content.replace(/\(https.*<br>/g,"");
+        }
+        if (url.indexOf("aikanshu") > 0) {
+            content = content.replace(/<img.*?src="\/novel\/images.*?>/g, "");
+        }
+        if (url.indexOf("ciweimao") > 0) {
+            content = content.replace(/<span>.*?<\/span>/g, "");
+            content = content.replace(/<\/?p.*?>/g, "");
+        }
+        content = content.replace(/&(nbsp|amp|quot|lt|gt|bp|emsp);/g, "");
+        content = content.replace(/(<br\/?>)+/g,"<br>");
+        content = content.replace(/<\/p><br><br><p>/g,"<br><br>");
+        content = content.replace(/ ([,\.!\?”]+)/g,"$1");
+        content = content.replace(/ ([,\.!\?”]+)/g,"$1");
+        //Đã lọc rác có thể. còn nhiều vcl ra ý. tự lọc đi
+        return Response.success(content);
+    }
+    return Response.error;
 }
-
 
