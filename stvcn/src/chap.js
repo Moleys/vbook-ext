4008afe4432efb6c726a73e96c62b8df7b3d70bc
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-07-06 10:56:56 +0700

    fix stv

--- stvcn/src/chap.js
+++ stvcn/src/chap.js
@@ -5,20 +5,21 @@ function execute(url) {
     var browser = Engine.newBrowser() // Khởi tạo browser
     let doc = browser.launch(url, 5500) // Mở trang web với timeout, trả về Document object
     el1 = doc.select(".contentbox")
-    el1.select("i[hd=\"\"]").remove()
-
     browser.close()
     // console.log(el1)
+    el1.select("i[hd=\"\"]").remove()
     el1.select("img").remove()
     el1.select("span").remove()
+    el1.select("a").remove()
     el1 = el1.html()
     el1 = el1.replace(/\"/g,"'")
 
-    console.log(el1)
-
-    const regex1 = /<i h='(.*?)' t='(.*?)' v='(.*?)' p='(.*?)' id='(.*?)'>(.*?)<\/i>/g;
-    const regex2 = /<i (.*?)>(.*?)<\/i>/g;
-    el1 = el1.replace(regex1, "$2").replace(regex2, "").replace(/ /g,"").replace("Đọctrênwebđểcóchấtlượngdịchcaovàủnghộwebsite.", "")
+    const regex1 = /<i(.*?)t='(.*?)'(.*?)'>(.*?)<\/i>/g;
+    el1 = el1.replace(regex1, "$2").replace(/ /g,"").replace("Đọctrênwebđểcóchấtlượngdịchcaovàủnghộwebsite.", "")
+    if (url.indexOf("uukanshu") > 0) {
+        el1.replace(/[UＵ][UＵ]\s*看书\s*[wｗ][wｗ][wｗ][\.．][uｕ][uｕ][kｋ][aａ][nｎ][sｓ][hｈ][uｕ][\.．][cｃ][oｏ][mｍ]/gi, "");
+    }
+    
     if(el1.includes("Đangtảinộidungchương")){
         el1 = "Lấy nội dung thất bại, vui lòng load lại chap"
     }
