570d6955320771a5d41bc071b6cdaad7b5b7ad55
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-07-16 14:33:58 +0700

    delete website die, VN cant access

--- netxsw/src/home.js
+++
@@ -1,5 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "首页", input: "https://www.netxsw.com/", script: "gen.js"}
-    ]);
-}
--- vipkanshu/src/home.js
+++
@@ -1,6 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "首页", input: "https://www.vipkanshu.vip/", script: "gen.js"}
-
-    ]);
-}
