244fd17222bc0f198bef527e6b2814708975ee74
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-07-20 16:55:15 +0700

    stvbr

---
+++ stvbr/src/gen.js
@@ -0,0 +1,23 @@
+function execute(url, page) {
+    if (!page) page = '1';
+    let reponse = fetch(url+'&p='+page)
+    if(reponse.ok){
+        let doc = reponse.html()
+        let next = doc.select(".pagination").select("li.active + li").text()
+        let el = doc.select("#searchviewdiv a.booksearch")
+        let data = [];
+        el.forEach(e => {
+            let img = e.select("img").first().attr("src");
+            if(img.startsWith('//')) img = img.replace('//','https://')
+            data.push({
+                name: e.select(".searchbooktitle").first().text(),
+                link: e.select("a").first().attr("href"),
+                cover: img || 'https://i.imgur.com/KP0Z6Eh.png',
+                description: e.select(".searchtag").first().text(),
+                host: "https://sangtacviet.me"
+            })
+        });
+        return Response.success(data, next)
+    }
+    return null;
+}
---
+++ stvbr/src/search.js
@@ -0,0 +1,21 @@
+function execute(key, page) {
+    if (!page) page = '1';
+    let response = fetch('https://sangtacviet.me/?find=&findinname='+key+'&minc=0&tag=&p='+page);
+    if(response.ok){
+        let doc = response.html()
+        let next = doc.select(".pagination").select("li.active + li").text()
+        let el = doc.select("#searchviewdiv a.booksearch")
+        let data = [];
+        el.forEach(e => {
+            data.push({
+                name: e.select(".searchbooktitle").first().text(),
+                link: e.select("a").first().attr("href"),
+                cover: e.select("img").first().attr("src"),
+                description: e.select(" div > span.searchtag").last().text(),
+                host: "https://sangtacviet.me"
+            })
+        });
+        return Response.success(data, next)
+    }
+    return null;
+}
