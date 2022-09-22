96db673aaafca8145c65b8113afaf5bfa2bce499
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-09-22 20:37:32 +0700

    f mijiashe

--- mijiashe/src/chap.js
+++ mijiashe/src/chap.js
@@ -1,12 +1,23 @@
 function execute(url) {
     url = url.replace('m.mijiashe.com', 'www.mijiashe.com');
-    let response = fetch(url);
-
-    if (response.ok) {
-        let doc = response.html();
-        let htm = doc.select("#content").html();
-        htm = htm.replace(/\&nbsp;/g, "");
-        return Response.success(htm);
+    let cvData ="";
+    let part1 = url.replace("http://www.mijiashe.com", "").replace("https://www.mijiashe.com", "").replace(".html","");
+    var next = part1;
+    while (next.includes(part1)) {
+        let response = fetch("http://www.mijiashe.com" + next +".html");
+        if (response.ok) {
+            let doc = response.html();
+            next = doc.select("a#next_url").attr("href").replace(".html","");
+            doc.select("span").remove();
+            let htm = doc.select("#content").html();
+            htm = htm.replace(/\&nbsp;/g, "");
+            cvData = cvData + htm;
+        } else {
+            break;
+        }
+    }
+    if (cvData) {
+        return Response.success(cvData);
     }
     return null;
 }
