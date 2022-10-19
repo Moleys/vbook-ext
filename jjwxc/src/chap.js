a8e3da3bc7e857994a4dca1569f45aad172ea189
Mol <34809211+Moleys@users.noreply.github.com>
2022-10-19 15:35:54 +0700

    Jjwxc

--- jjwxc/src/chap.js
+++ jjwxc/src/chap.js
@@ -4,13 +4,14 @@ function execute(url) {
     let chapterid = url1.split("&chapterid=")[1];
     let novelid =  url1.split("&chapterid=")[0];
     let response = fetch("http://app-cdn.jjwxc.net/androidapi/chapterContent?novelId="+novelid+"&chapterId="+chapterid);
+    console.log("http://app-cdn.jjwxc.net/androidapi/chapterContent?novelId="+novelid+"&chapterId="+chapterid)
     if (response.ok) {
         let res_json = response.json();
         if(res_json.message){
             var html1 = "Đây là chương VIP. Nếu muốn đọc bạn cần mua chương VIP rồi đăng nhập tài khoản vào trình duyệt của Vbook.<br>Nếu bạn vừa mới đăng nhập thì reload - tải lại chương này để cập nhật nội dung.<br>Nếu vẫn không đọc được thì lập chủ đề bên Góp ý báo lỗi!";
             return Response.success(html1);
         }
-        let chap_content = res_json.content.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/<br><br>/g, "<br>");
+        let chap_content = res_json.content.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/\n　　/g,"<br>").replace(/<br><br>/g, "<br>");
         let sayBody = res_json.sayBody
         console.log(sayBody)
         if(sayBody.length>0){
