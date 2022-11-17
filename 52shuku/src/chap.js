29c3c44fe7d0e622e6ce5e2cb9e48a87ada3f7d7
Mol <34809211+Moleys@users.noreply.github.com>
2022-11-17 17:52:38 +0700

    fix 52shuku lwread

--- 52shuku/src/chap.js
+++ 52shuku/src/chap.js
@@ -2,8 +2,9 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        let htm = doc.select("article.article-content").html().split('<div class=\"pagination2\">')[0]+"<\/p>";
-        htm = Html.clean(htm, ["p"]).replace(/<p><\/p>/g,"")
+        let htm = doc.select("article.article-content").html()
+        // htm = htm.split('<div class=\"pagination2\">')[0]+"<\/p>";
+        // htm = Html.clean(htm, ["p"]).replace(/<p><\/p>/g,"")
         return Response.success(htm);
     }
     return null;
