a98dc5ead0d5e4306e39bb3965904e8496faf137
Mol <34809211+Moleys@users.noreply.github.com>
2022-12-18 15:02:59 +0700

    novel543

--- novel543/src/chap.js
+++ novel543/src/chap.js
@@ -1,19 +1,25 @@
 function execute(url) {
     url = url.replace('m.novel543.com', 'www.novel543.com');
-    let response = fetch(url);
-
-    if (response.ok) {
-        let doc = response.html();
-        doc.select("script").remove();
-        doc.select(".adsbygoogle").remove();
-        doc.select(".adBlock").remove();
-
-        
-
-
-        let htm = doc.select(".content").html();
-        htm = htm.replace(/\&nbsp;/g, "");
-        return Response.success(htm);
+    let cvData ="";
+    let part1 = url.replace("http://www.novel543.com", "").replace("https://www.novel543.com", "").replace(".html","");
+    var next = part1;
+    while (next.includes(part1)) {
+        let response = fetch("https://www.novel543.com" + next +".html");
+        if (response.ok) {
+            let doc = response.html();
+            doc.select("script").remove();
+            doc.select(".adsbygoogle").remove();
+            doc.select(".adBlock").remove();
+            let htm = doc.select(".content").html();
+            htm = htm.replace(/\&nbsp;/g, "");
+            next = doc.select("a:contains(下一章)").first().attr("href").replace(".html","");
+            cvData = cvData + htm;
+        } else {
+            break;
+        }
+    }
+    if (cvData) {
+        return Response.success(cvData);
     }
     return null;
 }
