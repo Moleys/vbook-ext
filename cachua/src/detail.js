68571331059409aca11f363838ebbb999b7f075c
Mol <34809211+Moleys@users.noreply.github.com>
2023-03-29 10:54:49 +0700

    cachua

--- cachua/src/detail.js
+++ cachua/src/detail.js
@@ -3,7 +3,7 @@ function execute(url) {
         url = url.slice(0, -1)
     let bookid = url.split(/[/ ]+/).pop()
     // https://api3-normal-lf.fqnovel.com/reading/bookapi/directory/all_items/v/?need_version=true&book_id=" +bookid + "&iid=2159861899465991&aid=1967&app_name=novelapp&version_code=311
-    let newurl = "https://api3-normal-lf.fqnovel.com/reading/bookapi/directory/all_items/v/?need_version=true&book_id=" +bookid + "&iid=2159861899465991&aid=1967&app_name=novelapp&version_code=311"
+    let newurl = "https://api5-normal-lf.fqnovel.com/reading/bookapi/directory/all_items/v/?need_version=true&book_id=" +bookid + "&iid=2159861899465991&aid=1967&app_name=novelapp&version_code=311"
     let response = fetch(newurl, {
         headers: {
             'user-agent': UserAgent.android()
