e52184279cddda9a1b14ff4ab3118cb1fb2ae66e
Mol <34809211+Moleys@users.noreply.github.com>
2023-04-29 10:56:50 +0700

    jjwxc

--- jjwxc/src/chap.js
+++ jjwxc/src/chap.js
@@ -1,22 +1,67 @@
+load('crypto.js');
 
 function execute(url) {
-    let url1 = url.split("?novelid=")[1];
-    let chapterid = url1.split("&chapterid=")[1];
-    let novelid =  url1.split("&chapterid=")[0];
-    let response = fetch("http://app-cdn.jjwxc.net/androidapi/chapterContent?novelId="+novelid+"&chapterId="+chapterid);
-    console.log("http://app-cdn.jjwxc.net/androidapi/chapterContent?novelId="+novelid+"&chapterId="+chapterid)
-    if (response.ok) {
-        let res_json = response.json();
-        if(res_json.message){
-            var html1 = "Đây là chương VIP. Nếu muốn đọc bạn cần mua chương VIP rồi đăng nhập tài khoản vào trình duyệt của Vbook.<br>Nếu bạn vừa mới đăng nhập thì reload - tải lại chương này để cập nhật nội dung.<br>Nếu vẫn không đọc được thì lập chủ đề bên Góp ý báo lỗi!";
+
+    if(url.includes("token"))
+    {
+        const token = url.split("token=")[1];
+        let html1 = `Token không hợp lệ. Bạn chưa đăng nhập Tấn Giang.<br/>Hãy tìm đọc bài viết “[Tấn Giang, jjwxc] Hướng dẫn lấy token trong app và đọc truyện trên Vbook” và làm theo hướng dẫn. Sau khi lưu token nhớ load lại mục lục truyện.`;
+        if(token === "error" || token.length < 30)
             return Response.success(html1);
+        else {
+
+            let response1 = fetch(url);
+            if (response1.ok) {
+                let res_json1 = response1.json().downloadContent[0];
+
+                if(res_json1.message){
+                    html1 = "Đây là chương VIP. Nếu muốn đọc bạn cần mua chương VIP ở Tấn Giang.<br>Nếu bạn vừa mới mua thì reload - tải lại chương này, để cập nhật nội dung.<br>Nếu vẫn không đọc được thì lập chủ đề bên Góp ý báo lỗi!";
+                    return Response.success(html1);
+                }
+                else {
+                    let sayBody = res_json1.sayBody
+                    let chap_content = res_json1.content
+                    chap_content = decryptContent(chap_content)
+                    chap_content = getConent(chap_content,sayBody);
+                    return Response.success(chap_content);             
+                }
+            }
+
         }
-        let chap_content = res_json.content.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/\n　　/g,"<br>").replace(/<br><br>/g, "<br>");
-        let sayBody = res_json.sayBody
-        console.log(sayBody)
-        if(sayBody.length>0){
-            chap_content = chap_content + "<br>-------------<br>" + sayBody.replace(/\r\n/g,"<br>")
-        }
-        return Response.success(chap_content);
     }
+    else
+    {
+        // chap free
+        let response = fetch(url);
+        if (response.ok) {
+            let res_json = response.json();
+            let sayBody = res_json.sayBody
+            let chap_content = res_json.content
+            return Response.success(getConent(chap_content,sayBody));
+        }
+    }
+}
+
+function decryptContent(content) {
+    // Được hỗ trợ bởi thám tử Đạt 009
+    if (content.length <= 30) {
+        return "";
+    }
+    const key = CryptoJS.enc.Utf8.parse("KW8Dvm2N");
+    const iv = CryptoJS.enc.Utf8.parse("1ae2c94b");
+    const ciphertext = CryptoJS.enc.Base64.parse(content);
+    const decrypted = CryptoJS.DES.decrypt({ ciphertext }, key, {
+        iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+        });
+    return decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+function getConent(chap_content, sayBody) {
+    chap_content = chap_content.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/\n　　/g,"<br>").replace(/<br><br>/g, "<br>");
+    if(sayBody.length>0){
+        chap_content = chap_content + "<br>-------------<br>" + sayBody.replace(/\r\n/g,"<br>")
+    }
+    return chap_content;
 }
--- jjwxc/src/toc.js
+++ jjwxc/src/toc.js
@@ -1,40 +1,39 @@
 function execute(url) {
-    var bookID = '0';
-    if(url.indexOf("book2") !== -1)
-    {
-        bookID = url.split(/[/ ]+/).pop();
+    const match = url.match(/(?:book2|novelid=)([^/&]+)/);
+    const bookID = match ? match[1] : "";
+    let token = "error"
+    let response_tk = fetch("https://moldich.gq/tools/jjwxc-sign.php");
+    if (response_tk.ok) {
+        let doc = response_tk.html();
+        token = doc.select("#token_display").text().trim()
+        token = (token === "Chưa thiết lập") ? "error" : token;
     }
-    if(url.indexOf("novelid=") !== -1)
-    {
-        if(url.slice(-1) === "/")
-	        url = url.slice(0, -1)
-        bookID = url.split("novelid=")[1]
-        if(url.indexOf("&chapterid=") !== -1){
-            bookID = bookI.split("&chapterid=")[0];
-        }
-    }
-    let url1 = "https://app-cdn.jjwxc.net/androidapi/chapterList?novelId="+ bookID +"&more=0&whole=1";
+    console.log(token)
 
+
+    let url1 = "https://app-cdn.jjwxc.net/androidapi/chapterList?novelId="+ bookID +"&more=0&whole=1";
     let response = fetch(url1);
     if (response.ok) {
         let el = response.json().chapterlist;
         const data = [];
         for (let i = 0;i < el.length; i++) {
             let name =  el[i].chaptername;
-
             if(el[i].chaptertype != '1')
             {
                 name = el[i].chapterid +". " + name.trim();
-                let link = "http://www.jjwxc.net/onebook.php?novelid="+ bookID +"&chapterid=" + el[i].chapterid;
+                let chapterid=  el[i].chapterid
+                let link = 'https://app.jjwxc.net/androidapi/chapterContent?novelId='+bookID+'&chapterId='+chapterid;
+                let host = "http://app.jjwxc.net"
                 let checkVIP = el[i].isvip;
                 if(checkVIP>0) {
                     name =  name + " [VIP]";
-                    link = "http://my.jjwxc.net/onebook_vip.php?novelid=" + bookID + "&chapterid=" + el[i].chapterid;
+                    link = "https://android.jjwxc.net/androidapi/androidChapterBatchDownload?versionCode=290&novelId="+bookID+"&chapterIds="+chapterid+"&token="+token;
+                    host = "https://android.jjwxc.net"
                 }
                 data.push({
                     name: name,
                     url: link ,
-                    host: "http://www.jjwxc.net"
+                    host: host
                 })
 
             }
