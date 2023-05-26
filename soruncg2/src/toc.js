33306f614783e07b35007ce5cdca949c936d5c3c
Mol <34809211+Moleys@users.noreply.github.com>
2023-05-26 11:40:48 +0700

    fix

--- soruncg2/src/toc.js
+++ soruncg2/src/toc.js
@@ -1,4 +1,6 @@
 load('crypto.js');
+load('decode.js');
+
 function execute(url) {
     let newurl = url.replace("/info.html","/")
     let response = fetch(newurl);
@@ -20,30 +22,4 @@ function execute(url) {
         return Response.success(data);  
     }
     return null;
-}
-
-
-
-function decode_text(input) {
-    const key = CryptoJS.enc.Utf8.parse('OW84U8Eerdb99rtsTXWSILDO');
-    const iv = CryptoJS.enc.Utf8.parse('SK8bncVu');
-    const algorithm = CryptoJS.TripleDES;
-    const padding = CryptoJS.pad.Pkcs7;
-
-    let result = input;
-
-    try {
-    const matches = result.match(/"[^"]*[{}]{2,}[^"]+"/g);
-
-    for (let i in matches) {
-        const match = matches[i].match(/"([^"]*[{}]{2,})([^"]+)"/);
-        const xx = CryptoJS.TripleDES.decrypt(match[2], key, { iv: iv, padding: padding }).toString(CryptoJS.enc.Utf8);
-        result = result.replace(match[1] + match[2], xx);
-    }
-    } catch (error) {
-    // Handle error if needed
-    console.error(error);
-    }
-
-    return result;
 }
