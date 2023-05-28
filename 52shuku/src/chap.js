d9567944d79bef29dd0450445a8bb88ea0c3aede
Đường đen <79037079+duongden@users.noreply.github.com>
2023-05-28 11:39:58 +0700

    update chap.js
    xoá dòng giới thiệu cuối chap

--- 52shuku/src/chap.js
+++ 52shuku/src/chap.js
@@ -2,10 +2,27 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        let htm = doc.select("article.article-content").html()
-        // htm = htm.split('<div class=\"pagination2\">')[0]+"<\/p>";
-        // htm = Html.clean(htm, ["p"]).replace(/<p><\/p>/g,"")
-        return Response.success(htm);
+        doc.select("lineCorrect").remove();
+        let htm = doc.select("#text").html()
+        return Response.success(cleanHtml(htm));
     }
     return null;
+}
+
+function cleanHtml(html) {
+    // replace p tags with empty string
+    html = html.replace(/<\/?p>/g, "");
+    // replace a tags with empty string
+    html = html.replace(/<a[^>]*>([^<]+)<\/a>/g, '');
+    // replace new line characters with br tags
+    html = html.replace(/\n/g, '<br>');
+    // remove duplicate br tags
+    html = html.replace(/(<br>\s*){2,}/gm, '<br>');
+    // strip html comments
+    html = html.replace(/<!--[^>]*-->/gm, '');
+    // html decode
+    html = html.replace(/&(nbsp|amp|quot|lt|gt);/g, "");
+    // trim br tags
+    html = html.replace(/(^(\s*<br>\s*)+|(<br>\s*)+$)/gm, '');
+    return html.trim();
 }
