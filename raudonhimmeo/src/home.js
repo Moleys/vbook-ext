798439f9d07d35ffb067aa6540c77507c2534d32
Mol <34809211+Moleys@users.noreply.github.com>
2023-05-30 22:53:22 +0700

    raudo

--- raudonhimmeo/src/home.js
+++ raudonhimmeo/src/home.js
@@ -1,14 +1,8 @@
-function execute() {
-    return Response.success([
-        { title: "truyencv", input: "truyencv", script: "gen.js" },
-        { title: "tangthuvien", input: "tangthuvien", script: "gen.js" },
-        { title: "wattpad", input: "wattpad", script: "gen.js" },
-        { title: "html5qq", input: "html5qq", script: "gen.js" },
-        { title: "jiaston", input: "jiaston", script: "gen.js" },
-        { title: "qidian", input: "qidian", script: "gen.js" },
-        { title: "fanqienovel", input: "fanqienovel", script: "gen.js" },
-        { title: "uukanshu", input: "uukanshu", script: "gen.js" },
-        { title: "69shu", input: "69shu", script: "gen.js" },
-        { title: "17k", input: "17k", script: "gen.js" },
-    ]);
+function execute(url) {
+    let response = fetch("https://raudo.eu.org/api/vbook.php")
+    if (response.ok) {
+        let json = response.json();
+        return Response.success(json);
+    }
+    return null;
 }
