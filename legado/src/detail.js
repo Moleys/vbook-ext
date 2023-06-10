5126d1c9a118f188b7e9cc41ae2aae062d6ba6a1
Mol <34809211+Moleys@users.noreply.github.com>
2023-06-10 19:59:26 +0700

    legado

--- legado/src/detail.js
+++ legado/src/detail.js
@@ -1,27 +1,41 @@
 
 function execute(url) {
+    let host = "http://192.168.0.102:1122"
     let book_url = decodeURIComponent(url.split("/getChapterList?url=")[1])
-    let response = fetch(host+"/getBookshelf")
-    if (response.ok) {
-        let json = response.json();
-        let book_list = json.data;
-        // let book_info = book_list.find(obj => obj.bookUrl === book_url);
-        let book_info;
-        for (let i = 0; i < book_list.length; i++) {
-        if (book_list[i].bookUrl == book_url) {
-            book_info = book_list[i];
-            break;
-        }
-        }
 
-        return Response.success({
-            name: book_info.name,
-            cover:  book_info.coverUrl,
-            author: book_info.author,
-            description: book_info.intro.replace(/\r\n/g, "<br>"),
-            detail: "作者：" + book_info.author + "<br>" + book_info.kind,
-            host: "https://raudo.nhimmeo.cf",
-        });
-    }
-    return null;
+    if (typeof host === "undefined")
+        {
+            return Response.error(book_url);
+
+        }
+        else
+        {
+            let response = fetch(host+"/getBookshelf")
+            if (response.ok) {
+try {
+                let json = response.json();
+                let book_list = json.data;
+                // let book_info = book_list.find(obj => obj.bookUrl === book_url);
+                let book_info;
+
+                for (let i = 0; i < book_list.length; i++) {
+                    if (book_list[i].bookUrl == book_url) {
+                        book_info = book_list[i];
+                        break;
+                    }
+                }
+
+                return Response.success({
+                    name: book_info.name,
+                    cover:  book_info.coverUrl,
+                    author: book_info.author,
+                    description: book_info.intro.replace(/\r\n/g, "<br>"),
+                    detail: "作者：" + book_info.author + "<br>" + book_info.kind,
+                    host: "https://raudo.nhimmeo.cf",
+                });
+}catch (error) {
+                return Response.error(error);
+            }}
+            return null;
+        }
 }
