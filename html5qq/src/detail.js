b36a2ef5b192a7df2009e536876132cc69af3fd6
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-07-05 17:40:01 +0700

    fix html5

--- html5qq/src/detail.js
+++ html5qq/src/detail.js
@@ -2,11 +2,17 @@ function execute(url) {
     const bookidRegex = /bookid=(\d+)/;
     const match = url.match(bookidRegex);
     const bookid = match[1];
-    let url2 = "https://bookshelf.html5.qq.com/qbread/api/novel/adbooks/bookinfo?bookid=" + bookid
-    let response = fetch(url2, {"headers":{"Referer":"https://bookshelf.html5.qq.com/qbread/adread/catalog"}});
+    let url2 = "https://bookshelf.html5.qq.com/qbread/api/novel/intro-info?bookid=" + bookid
+    let response = fetch(url2, {
+        "headers":
+        {
+        "user-agent":"Mozilla/5.0 (Linux; Android 10; MI 8 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.101 Mobile Safari/537.36",
+        "Referer":"https://bookshelf.html5.qq.com/qbread"
+        }
+    });
     if (response.ok) {
         let doc = response.json();
-        let book = doc.data
+        let book = doc.data.bookInfo
         return Response.success({
             name: book.resourceName,
             cover: book.picurl,
