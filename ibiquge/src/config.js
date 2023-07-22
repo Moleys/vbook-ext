b91057fc9330f89c19b9d3600848437419cc71d2
Mol <34809211+Moleys@users.noreply.github.com>
2023-07-22 21:30:58 +0700

    ibiquge

--- ibiquge/src/config.js
+++
@@ -1,4 +0,0 @@
-let base_url = 'https://www.nettruyenmax.com';
-if (typeof config_url !== "undefined") {
-    base_url = config_url
-}
--- ibiquge/src/home.js
+++
@@ -1,6 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "首页", input: "http://www.ibiquzw.com/", script: "gen.js"}
-
-    ]);
-}
