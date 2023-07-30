8b6c6f192ca19d3e733a4510330160df76cd9f12
Mol <34809211+Moleys@users.noreply.github.com>
2023-07-30 09:35:29 +0700

    fqweb

--- fqweb/src/chap.js
+++ fqweb/src/chap.js
@@ -1,4 +1,12 @@
 function execute(url) {
+    if (typeof host !== "undefined"  && host.includes("api-fanqienovel.sunianyun.live")) {
+        let item_id = url.split("item_id=")[1]
+        let response_host = fetch("http://list.api-fanqienovel.sunianyun.live/random?item_id=" + item_id)
+        if (response_host.ok) {
+            url = response_host.text() + "/content?item_id=" + item_id
+        }
+    }
+    
     let response_chapter_info = fetch(url)
     if (response_chapter_info.ok) {
         let json = response_chapter_info.json();
@@ -6,5 +14,8 @@ function execute(url) {
         return Response.success(chapter_info);
     }
     return Response.error("Kiểm tra lại app Fanqie");
+
+
+
 }
 
