50ed71237323774e42e9894b29aa876af25f91af
Mol <34809211+Moleys@users.noreply.github.com>
2023-08-30 16:05:29 +0700

    sv2 nhimmeo

--- nhimmeo_sv2/src/detail.js
+++ nhimmeo_sv2/src/detail.js
@@ -16,7 +16,7 @@ function execute(url) {
             author: book_info.author_name,
             description: "<br>题材：" +book_info.tag + "<br>简介：<br>" + book_info.description.replace(/\r\n/g, "<br>"),
             detail: "作者：" + book_info.author_name + "<br>更新：" +book_info.uptime+ "<br>字数：" +book_info.total_word_count,
-            host: "https://sv2.nhimmeo.cf"
+            host: "https://sv2.nhimmeo.ovh"
         });
     }
     return null;
