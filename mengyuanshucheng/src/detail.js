19e767d70aa8ae8a28260267a2962ab15588ded4
Đường đen <79037079+duongden@users.noreply.github.com>
2023-09-19 20:29:26 +0700

    update domain
    https://www.mengyuanshuchengcn.com/

--- mengyuanshucheng/src/detail.js
+++ mengyuanshucheng/src/detail.js
@@ -1,22 +1,29 @@
 function execute(url) {
-    url = url.replace('wap.mengyuanshucheng.com', 'www.mengyuanshucheng.com');
+    url = url.replace('wap.mengyuanshuchengcn.com', 'www.mengyuanshuchengcn.com');
     url = url.replace("/index/","/my/")
     let response = fetch(url);
     if (response.ok) {
-
         let doc = response.html();
-        let coverImg = doc.select("#bookimg img").first().attr("src");
+        let coverImg = doc.select('meta[property="og:image"]').attr("content");
+        let descriptionMeta = doc.select('meta[property="og:description"]').attr("content").replace("r>","").replace("《",'"').replace("》",'"');
+
+        let title = doc.select('meta[property="og:title"]').attr("content");
+        let status = doc.select('meta[property="og:novel:status"]').attr("content");
+        let newChap = doc.select('meta[property="og:novel:latest_chapter_name"]').attr("content");
+        let author = doc.select('meta[property="og:novel:author"]').attr("content");
+        let category = doc.select('meta[property="og:novel:category"]').attr("content");
+        let updateTime = doc.select('meta[property="og:novel:update_time"]').attr("content").replace(/\d\d:\d\d:\d\d/g, "");
+
         if (coverImg.startsWith("/")) {
-            coverImg = "http://www.mengyuanshucheng.com" + coverImg;
+            coverImg = "https://www.mengyuanshuchengcn.com" + coverImg;
         }
-        let author = doc.select(".bookright #author").first().text().replace(/作\s*者：/g, "")
         return Response.success({
-            name: doc.select(".bookright h1").text(),
+            name: title,
             cover: coverImg,
             author: author,
-            description: doc.select("#bookintro p").text(),
-            detail:  "作者：" + author + "<br>" +doc.select(".new_t span").last().html(),
-            host: "http://www.mengyuanshucheng.com"
+            description: descriptionMeta,
+            detail: "Thể loại: " + category + '<br>' + "Tình trạng: " + status + '<br>' + "Mới nhất: " + newChap  + '<br>' + "Thời gian cập nhật: " + updateTime,
+            host: "https://www.mengyuanshuchengcn.com"
         });
     }
     return null;
