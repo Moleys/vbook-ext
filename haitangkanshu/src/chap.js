1a06100002d2e3260bf31c24ff0f61385184a6c4
Mol <34809211+Moleys@users.noreply.github.com>
2023-10-19 21:22:58 +0700

    haitangkanshu

--- haitangkanshu/src/chap.js
+++ haitangkanshu/src/chap.js
@@ -10,10 +10,13 @@ function execute(url) {
         let htm = doc.select("#C0NTENT");
         htm.select("p[style=\"color:red;\"]").remove();
         htm = htm.html().replace("<p>收藏网址：www.haitangkanshu.comm<\/p>","").replace("<p>(＞人＜；)<\/p>","");
+
+        
         //1/2 text sau không bị mã hóa
-        let text0 = doc.select("script").html().split("$('#C0NTENT').html(d(\"")[1];
+        let text0 = doc.select("script").html().split(").html(d(\"")[1];
         let a = text0.split("\", \"")[0].trim().replace(/\\/g,"");
         let b = text0.split("\", \"")[1].split("\"));")[0].trim();
+
 
         htm = d(a,b);
 
