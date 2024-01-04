3802d61b7a81239ce6e861656cf4de3c3f746614
Mol <34809211+Moleys@users.noreply.github.com>
2024-01-04 12:07:12 +0700

    longma

--- longmabook/src/toc.js
+++ longmabook/src/toc.js
@@ -34,19 +34,20 @@ function execute(url) {
             if(name){
                 let cart = e.html()
                 if(cart.includes('<span uk-icon=\"cart\" uk-tooltip=\"\u6D77\u68E0\u5E63\u50F9\u683C\"><\/span><\/font>\u514D\u8CBB&nbsp; <font color=\"green\">')){
-                    cart = ""; //Free
+                    cart = false
                 }
                 else if(cart.includes("<b><font color=\"red\">**\u5DF2\u8CFC\u8CB7")){
-                    cart = "✔ "//"[Purchased] ";
+                    cart = false
                 }
                 else{
-                    cart = "❌ " //"[VIP] ";
+                    cart = true
                 }
                 let link =  e.select("a").attr("href")
                 if(link)
                     data.push({
-                        name: cart + name,
+                        name: name,
                         url: "https://ebook.longmabook.com" + link,
+                        pay: true,
                         host: "https://ebook.longmabook.com"
                     });
             }
