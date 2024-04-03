4c66785196ceaa0bd8d63e5b6fc02e6db99614dc
Mol <34809211+Moleys@users.noreply.github.com>
2024-04-03 13:22:43 +0700

    2

--- fqweb/src/chap.js
+++ fqweb/src/chap.js
@@ -9,7 +9,8 @@ function execute(url) {
     let response_chapter_info = fetch(url)
     if (response_chapter_info.ok) {
         let json = response_chapter_info.json();
-        let chapter_info = json.data.data.content.replace(/<br\s*\/?>|\n/g, "<br><br>");
+
+        let chapter_info = json.data.content.replace(/<br\s*\/?>|\n/g, "<br><br>");
         return Response.success(chapter_info);
     }
     return Response.error("Kiểm tra lại app Fanqie");
