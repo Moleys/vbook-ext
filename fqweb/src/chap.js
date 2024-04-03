65e170b7bac14d5c479c0c8101deaf5c73a56bd6
Mol <34809211+Moleys@users.noreply.github.com>
2024-04-03 13:18:41 +0700

    2

--- fqweb/src/chap.js
+++ fqweb/src/chap.js
@@ -4,7 +4,7 @@ function execute(url) {
     const regex = /(?:item_id=|\/)(\d+)$/;
     let chapid = url.match(regex)[1]
 
-    url = config_host2 + "/content?item_id=" + chapid
+    url = "https://fqg.fcachua.pp.ua/api.php/" + chapid
 
     let response_chapter_info = fetch(url)
     if (response_chapter_info.ok) {
