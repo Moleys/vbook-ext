05ad91b00d9d7adf2f054e12f909c48e8999825a
Mol <34809211+Moleys@users.noreply.github.com>
2024-09-29 21:54:42 +0700

    45

--- fqweb/src/chap.js
+++ fqweb/src/chap.js
@@ -8,6 +8,10 @@ function execute(url) {
     if (response_chapter_info.ok) {
         let json = response_chapter_info.json();
         let chapter_info = json.data.data.content.replace(/<br\s*\/?>|\n/g, "<br><br>");
+        if(chapter_info == "Đã dùng hết số chương cho phép, chi tiết vui lòng vào Trang nguồn.")
+        {
+            return Response.error("Đã dùng hết số chương cho phép, chi tiết vui lòng vào Trang nguồn.");
+        }
         return Response.success(chapter_info);
     }
 
