892765e2535a17e35d311296f0c89245234812fc
Mol <34809211+Moleys@users.noreply.github.com>
2025-04-21 14:30:43 +0700

    legado

--- legado/src/chap.js
+++ legado/src/chap.js
@@ -1,5 +1,11 @@
 load('config.js');
+
 function execute(url) {
+    
+    const bookUrl = getBookUrl(url);
+
+
+
     if(url.includes("&type=comic")) {
         let url_chap = url.split("/getBookContent")[1]
         let response_chapter_info = fetch(config_host + "/getBookContent" + url_chap)
@@ -19,9 +25,27 @@ function execute(url) {
         if (response_chapter_info.ok) {
             let json = response_chapter_info.json();
             let chapter_info = json.data.replace(/<br\s*\/?>|\n/g, "<br><br>");
+            chapter_info = replaceImageLinks(chapter_info, bookUrl);
+
             return Response.success(chapter_info);
         }
         return Response.error("Kiểm tra lại Web service Legado");
     }
 }
 
+// Tách thủ công giá trị của tham số "url="
+function getBookUrl(fullUrl) {
+    const match = fullUrl.match(/[?&]url=([^&]+)/);
+    return match ? match[1] : "";
+}
+
+// Hàm thay ảnh
+function replaceImageLinks(html, bookUrl) {
+    const baseImageUrl = "http://localhost:1122/image";
+    const width = 800;
+    return html.replace(/<img\s+src="([^"]+)"\s*\/?>/g, (match, imgPath) => {
+        const encodedPath = encodeURIComponent(imgPath);
+        const fullImageUrl = `${baseImageUrl}?path=${encodedPath}&url=${bookUrl}&width=${width}`;
+        return `<img src="${fullImageUrl}">`;
+    });
+}
