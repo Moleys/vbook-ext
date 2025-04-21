f45dcd94eb91a9cc4c85ea282801f1a437154298
Mol <34809211+Moleys@users.noreply.github.com>
2025-04-21 14:04:39 +0700

    pixiv

--- pixiv/src/chap.js
+++ pixiv/src/chap.js
@@ -6,6 +6,14 @@ function execute(url) {
     if (response.ok) {
         let doc = response.json()
         let obj = doc.body.content
+        let imgs = doc.body.textEmbeddedImages
+        obj = obj.replace(/\[uploadedimage:(\d+)\]/g, (match, id) => {
+        const img = imgs[id];
+        if (img && img.urls && img.urls["1200x1200"]) {
+            return `<img src="${img.urls["1200x1200"].replace("https://i.pximg.net","https://i0.wp.com/pximg.perennialte.ch")}" />`;
+        }
+        return match; // Nếu không tìm thấy ảnh, giữ nguyên tag
+        });
         htm = obj.replace(/\n/g,"<br>");
         return Response.success(htm);
     }
