390b63b2b9f2236f5c79cccbb14441345a3e5f9d
Mol <34809211+Moleys@users.noreply.github.com>
2025-07-14 19:25:01 +0700

    e

--- elysia/src/chap.js
+++ elysia/src/chap.js
@@ -5,10 +5,13 @@ function execute(url) {
     if (response.ok) {
 		let doc = response.html();
         let data = []
-
+        const cookie = response.request.headers.cookie
+        const auto_use_chapter_vip = cookie.includes('auto_share_chapter_vip=on');
+        let text1 = doc.select("article div[style*=font-size:20px]").text().trim()
+        console.log(text1.length)
         const isUC = doc.html().includes('有道友分享了这一章');
 
-        if(isUC){
+        if((text1.length == 0) || isUC){
             let res2 = fetch(url.replace('chap', 'shchap'))
             if(res2.ok){
                 let doc2 = res2.html()
