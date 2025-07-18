078f89bd992b3604f34cb31300d53166f4e2960e
Mol <34809211+Moleys@users.noreply.github.com>
2025-07-18 12:41:53 +0700

    Remove multiple plugin directories and their scripts
    Deleted the entire directories and all related files for the plugins: 520banxia, aahhss, aduosw, linovel, linovelib, and vietnovel. This includes their icons, plugin.json, plugin.zip, and all JavaScript source files. Also updated the main plugin.json to reflect these removals.

--- aahhss/src/config.js
+++
@@ -1,9 +0,0 @@
-let BASE_URL = 'https://aahhss.com';
-let BASE_URL1 = 'https://www.aahhss.com';
-try {
-    if (CONFIG_URL) {
-        BASE_URL = CONFIG_URL;
-        BASE_URL1 = CONFIG_URL;
-    }
-} catch (error) {
-}
--- vietnovel/src/home.js
+++
@@ -1,9 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Truyện Nhiều Người Đọc", input:  "https://www.vietnovel.com/truyen-sang-tac-nhieu-nguoi-doc/", script: "gen.js"},
-        {title: "Royal Ranking", input:  "https://www.vietnovel.com/royal-ranking/", script: "gen.js"},
-        {title: "Truyện Mới Nhất", input:  "https://www.vietnovel.com/truyen-sang-tac-moi-nhat/", script: "gen.js"},
-        {title: "Truyện Full", input:  "https://www.vietnovel.com/truyen-sang-tac-full/", script: "gen.js"},
-        {title: "Truyện Chọn Lọc", input:  "https://www.vietnovel.com/truyen-sang-tac-chon-loc/", script: "gen.js"}
-    ]);
-}
